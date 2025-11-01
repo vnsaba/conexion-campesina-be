179b2ec9f385860582ca7df98cacf992734d511c
Liceth Usma <juanitausma02@gmail.com>
2025-11-01 17:30:54 -0500

    docs: update migration data format to match API structure

--- apps/product-service/src/product-service.module.ts
+++ apps/product-service/src/product-service.module.ts
@@ -2,9 +2,8 @@ import { Module } from '@nestjs/common';
 
 import { ProductBaseModule } from './product-base/product-base.module';
 import { ProductOfferModule } from './product-offer/product-offer.module';
-import { UnitModule } from './unit/unit.module';
 
 @Module({
-  imports: [ProductBaseModule, ProductOfferModule, UnitModule],
+  imports: [ProductBaseModule, ProductOfferModule],
 })
 export class ProductServiceModule {}
